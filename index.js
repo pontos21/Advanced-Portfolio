2b05a16527c65cccf6be59db4777b80b0a2f773d
paul serafim <paul.serafim.3021@gmail.com>
2022-07-29 15:26:07 +1000

    shapes

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
+
+function moveBackground(event) {
+    const shape = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+    
+    for (let i = 0; i < shape.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shape[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px )`
+    }
+
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+       document.body.classList.remove("dark-theme") 
+    }
+    
+}
 
 function contact(event) {
     event.preventDefault();
@@ -22,7 +49,7 @@ function contact(event) {
     })
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false
